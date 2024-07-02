d6eccc921ccc48ee3385bc26e737017f26d638c9
Matt McKee <mmckee@neyarobotics.com>
2024-07-02 08:27:54 -0400

    adding my personal changes

---
+++ remove_unused.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo rm /usr/share/applications/excel.exe.desktop && \
+sudo rm /usr/share/applications/gethelp.exe.desktop && \
+sudo rm /usr/share/applications/iediagcmd.exe.desktop && \
+sudo rm /usr/share/applications/iexplore.exe.desktop && \
+sudo rm /usr/share/applications/iexplorer.desktop && \
+sudo rm /usr/share/applications/licensemanagershellext.exe.desktop && \
+sudo rm /usr/share/applications/lync.exe.desktop && \
+sudo rm /usr/share/applications/mip.exe.desktop && \
+sudo rm /usr/share/applications/msaccess.exe.desktop && \
+sudo rm /usr/share/applications/msoadfsb.exe.desktop && \
+sudo rm /usr/share/applications/msoasb.exe.desktop && \
+sudo rm /usr/share/applications/msoxmled.exe.desktop && \
+sudo rm /usr/share/applications/mspub.exe.desktop && \
+sudo rm /usr/share/applications/ms-teamsupdate.exe.desktop && \
+sudo rm /usr/share/applications/onenote.exe.desktop && \
+sudo rm /usr/share/applications/outlook.exe.desktop && \
+sudo rm /usr/share/applications/powerpnt.exe.desktop && \
+sudo rm /usr/share/applications/powershell.exe.desktop && \
+sudo rm /usr/share/applications/sdxhelper.exe.desktop && \
+sudo rm /usr/share/applications/skype.exe.desktop && \
+sudo rm /usr/share/applications/skypeserver.exe.desktop && \
+sudo rm /usr/share/applications/snippingtool.exe.desktop && \
+sudo rm /usr/share/applications/tabtip.exe.desktop && \
+sudo rm /usr/share/applications/wab.exe.desktop && \
+sudo rm /usr/share/applications/wabmig.exe.desktop && \
+sudo rm /usr/share/applications/windowspackagemanagerserver.exe.desktop && \
+sudo rm /usr/share/applications/winget.exe.desktop && \
+sudo rm /usr/share/applications/winword.exe.desktop && \
+sudo rm /usr/share/applications/wmplayer.exe.desktop
